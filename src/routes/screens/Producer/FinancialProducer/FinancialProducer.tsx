f8b58fe2c9db3faa6d263ed72463699337f7d259
DESKTOP-623A0I2\leonardo <Leonardoabras@hotmail.com>
2022-04-08 15:56:04 -0300

    Feat: Criado tela de Financial Producer

--- src/routes/screens/Producer/FinancialProducer/FinancialProducer.tsx
+++ src/routes/screens/Producer/FinancialProducer/FinancialProducer.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-
+import { Header, Button } from '../../../../components';
 // components
-import { StyledContainer, StyledTitle } from './styles';
+import {
+  StyledContainer,
+  StyledTitle,
+  StyledText,
+  StyledButtonNext,
+  StyledButtonText,
+  StyledContainerChaves,
+  StyledTextChaves
+} from './styles';
 
 const FinancialProducer: React.FC = () => {
+  const returnButtons = () => {
+    return (
+      <>
+        <StyledButtonNext>
+          <StyledButtonText>Configurar chave pix</StyledButtonText>
+        </StyledButtonNext>
+        <StyledButtonNext>
+          <StyledButtonText>Cadastre aqui a sua chave Pix</StyledButtonText>
+        </StyledButtonNext>
+      </>
+    );
+  };
   return (
-    <StyledContainer>
-      <StyledTitle>Financial Producer</StyledTitle>
-    </StyledContainer>
+    <>
+      <Header title='Financeiro' />
+      <StyledContainer>
+        <StyledTitle bold>Saldo Disponível</StyledTitle>
+        <StyledText>R$ 00,00</StyledText>
+        {returnButtons()}
+        <StyledContainerChaves>
+          <StyledTextChaves> Chaves Cadastradas </StyledTextChaves>
+        </StyledContainerChaves>
+      </StyledContainer>
+    </>
   );
 };
 
